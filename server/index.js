8c6187b2541ecdffede74b10abb53e35be9d5dc1
Rushikesh <50754786+RushiChavan-dev@users.noreply.github.com>
2023-07-07 00:10:10 -0400

    Ecommerce

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import bodyParser from "body-parser";
+import  Express  from "express";
+import cors from 'cors';
+import mongoose from "mongoose";
+const app = Express();
+import postRoutes from './routes/post.js';
+import authRoutes from './routes/auth.js';
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+
+app.use("/posts", postRoutes);
+app.use("/user", authRoutes);
+let url = "mongodb+srv://rushichavan33:i3HrWlaLl77ZuC2d@cluster0.e0zikpy.mongodb.net/?retryWrites=true&w=majority";
+const port = process.env.port || 5000;
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{ app.listen(port , ()=>console.log("Server is running")) })
+    .catch((err)=>{console.log(err.message)});
+
+// mongoose.set("useFindAndModify", false);    
